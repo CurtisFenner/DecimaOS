b0e23605c24c19944e7d16f93b89ca4f6e09788e
CurtisFenner <cfenneremail@gmail.com>
2014-12-25 00:22:57 -0500

    port_word_in in asmio

--- clua32/include/asmio.asm
+++ clua32/include/asmio.asm
@@ -4,5 +4,16 @@ _fun_port_byte_in:
 mov ebx, esp
 add ebx, 4 ; reference first parameter
 mov dx, [ebx]
+mov eax, 0
 in al, dx
+ret
+
+;@ char, port_word_in, int
+_fun_port_word_in:
+; Short? For port
+mov ebx, esp
+add ebx, 4 ; reference first parameter
+mov dx, [ebx]
+mov eax, 0
+in ax, dx
 ret
