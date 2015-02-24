c91bb699f90bbe6e0c1515f1f1a692176fdc5563
CurtisFenner <cfenneremail@gmail.com>
2015-02-23 21:21:35 -0500

    New compile.lua base

--- clua32/compile.lua
+++ clua32/compile.lua
@@ -1,3 +1,8 @@
 function compile(identifiers)
-	
+	print("\n\ncompile(",identifiers,")");
+	for i,v in pairs(identifiers) do
+		print("", i, "","",v);
+	end
+
+	return "\n\n\n; compile(identifiers)\n\n\n"
 end
